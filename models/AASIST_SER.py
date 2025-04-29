16332c9b62754daaae2a02439633e87c636022ce
Anna <anna@LAPTOP-RS1A55O3>
2025-04-29 15:17:36 +0300

    film inf

--- models/AASIST_SER.py
+++ models/AASIST_SER.py
@@ -2,32 +2,34 @@ import torch
 import torch.nn as nn
 import torchaudio
 import torchaudio.transforms as T
-import torch.nn.functional as F
 from .AASIST import Model
 from .ACRNN import acrnn
 
-# class FiLMBlock(nn.Module):
-#     def __init__(self, sv_dim, cm_dim, hidden_dim=128):
-#         super().__init__()
-#         self.cm_ln = nn.LayerNorm(cm_dim)
-#         self.condition_to_gamma_beta = nn.Sequential(
-#             nn.Linear(cm_dim, hidden_dim),
-#             nn.ReLU(),
-#             nn.BatchNorm1d(hidden_dim),
-#             nn.Linear(hidden_dim, 2 * sv_dim)
-#         )
-        
-#         self.sv_ln = nn.LayerNorm(sv_dim)
+class FiLMBlock(nn.Module):
+    def __init__(self, sv_dim, cm_dim, hidden_dim=128):
+        super().__init__()
+        self.cm_ln = nn.LayerNorm(cm_dim)
+        self.condition_to_gamma_beta = nn.Sequential(
+            nn.Linear(cm_dim, hidden_dim),
+            nn.ReLU(),
+            nn.BatchNorm1d(hidden_dim)
+        )
 
-#     def forward(self, e_sv, e_cm):
-#         e_cm_norm = self.cm_ln(e_cm)
-#         gamma_beta = self.condition_to_gamma_beta(e_cm_norm)  # (batch_size, 2 * sv_dim)
-#         gamma, beta = torch.chunk(gamma_beta, 2, dim=-1)      # (batch_size, sv_dim), (batch_size, sv_dim)
+        self.gamma_linear = nn.Linear(hidden_dim, sv_dim)
+        self.beta_linear = nn.Linear(hidden_dim, sv_dim)
         
-#         e_sv_norm = self.sv_ln(e_sv)
-#         e1 = gamma * e_sv_norm + beta
+        self.sv_ln = nn.LayerNorm(sv_dim)
 
-#         return e1
+    def forward(self, e_sv, e_cm):
+        e_cm_norm = self.cm_ln(e_cm)
+        gamma_beta = self.condition_to_gamma_beta(e_cm_norm)
+        gamma = self.gamma_linear(gamma_beta)
+        beta = self.beta_linear(gamma_beta)
+        
+        e_sv_norm = self.sv_ln(e_sv)
+        e_mod1 = gamma * e_sv_norm + beta
+
+        return e_mod1
 
 
 
@@ -40,8 +42,6 @@ class AASISTWithEmotion(nn.Module):
         # self.aasist.eval()
         # for p in self.aasist.parameters():
         #     p.requires_grad = False
-
-        
 
         self.ser = acrnn()
         # self.ser.load_state_dict(torch.load(ser_config["ser_path"]))
@@ -65,8 +65,19 @@ class AASISTWithEmotion(nn.Module):
 
         self.aasist_feat_dim = 5 * aasist_config["gat_dims"][1]
         self.ser_feat_dim = 256
-        self.test_linear = nn.Linear(self.aasist_feat_dim + self.ser_feat_dim, 2)
 
+        self.film = FiLMBlock(self.aasist_feat_dim, self.ser_feat_dim)
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(self.aasist_feat_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 2)
+        )
 
     def extract_mel_features(self, x):
         mel_spec = self.mel_transform(x)
@@ -79,17 +90,12 @@ class AASISTWithEmotion(nn.Module):
         with torch.no_grad():
             aasist_feat, _ = self.aasist(x, Freq_aug=Freq_aug)
             ser_feat = self.ser(self.extract_mel_features(x))
+
+        modulated_features = self.film(aasist_feat, ser_feat)
         
-        aasist_feat = F.normalize(aasist_feat, p=2, dim=1)
-        ser_feat = F.normalize(ser_feat, p=2, dim=1)
+        output = self.classifier(modulated_features)
         
-        combined_feat = torch.cat([aasist_feat, ser_feat], dim=1)
-        
-        combined_feat = F.normalize(combined_feat, p=2, dim=1)
-        
-        out = self.test_linear(combined_feat)
-        
-        return combined_feat, out
+        return modulated_features, output
 
     @property
     def device(self):
